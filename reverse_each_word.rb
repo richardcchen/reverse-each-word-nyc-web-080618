e02214559d5b815a0438f09fc8bf25923ab37c90
Richard Chen <richard.c.chen@gmail.com>
2018-07-23 02:29:16 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   revarray = []
   counter = 0
-  array.each do |word|
+  array.collect do |word|
     revarray[counter] = word.reverse!
     counter += 1
   end
